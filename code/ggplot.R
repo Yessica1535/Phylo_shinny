c38ebac89ab4e9fd79ab2f913647ffb62230bcbd
Yessica1535 <1907553f@umich.mx>
2024-10-30 10:03:22 -0600

    Update ggplot.R

--- code/ggplot.R
+++ code/ggplot.R
@@ -2,31 +2,31 @@
 library(shiny)
 library(ggplot2)
 
-# Sample OTU data (replace this with your actual data)
-otu_data <- data.frame(
-  OTU = c("OTU_1", "OTU_2", "OTU_3", "OTU_4"),
-  Sample_1 = c(120, 90, 60, 234),
-  Sample_2 = c(250, 100, 75, 125),
-  Sample_3 = c(180, 95, 80, 345),
-  Sample_4 = c(300, 120, 90, 78),
-  Sample_5 = c(200, 110, 85, 80)
-)
+# Sample ASV data 
+#asv_data <- data.frame(
+#            ASV = c("ASV_1", "ASV_2", "ASV_3", "ASV_4"),
+#  zr2757_10V3V4 = c(37, 55, 8, 47) 
+#   zr2757_1V3V4 = c(250, 100, 75, 125),
+#   zr2757_2V3V4 = c(180, 95, 80, 345),
+#   zr2757_3V3V4 = c(300, 120, 90, 78),
+#   zr2757_4V3V4 = c(200, 110, 85, 80)
+#)
 
+# Read CSV file as a data frame
+asv_data <- read.csv("C:/Users/yessi/OneDrive/Documentos/Calakmul/data/OtuTable.csv", header = TRUE)
 
 # Reshape data to long format for ggplot
-otu_long <- reshape2::melt(otu_data, id.vars = "OTU", variable.name = "Sample", value.name = "Read_Count")
+asv_long <- reshape2::melt(asv_data, id.vars = "ASV", variable.name = "Sample", value.name = "Read_Count")
 
-# Filter data for the selected OTU
-filtered_data <- otu_long[otu_long$OTU == "OTU_1", ]
+# Filter data for the selected ASV
+filtered_data <- asv_long[asv_long$ASV == "ASV_1", ]
 
 # Create a grouping variable for samples
-filtered_data$Group <- ifelse(filtered_data$Sample %in% c("Sample_1", "Sample_2"), "Group 1", "Group 2")
+filtered_data$Group <- ifelse(filtered_data$Sample %in% c("zr2757_1V3V4", "zr2757_2V3V4"), "Group 1", "Group 2")
 
 # Generate the boxplot using ggplot
 ggplot(filtered_data, aes(x = Group, y = Read_Count, fill = Group)) +
   geom_boxplot() +
-  labs(title = paste("Boxplot for", "OTU_1"),
+  labs(title = paste("Boxplot for", "ASV_1"),
        x = "Group", y = "Read Count") +
   scale_fill_manual (values = c("Group 1" = "#B57EDC", "Group 2" = "#BFF7DC")) + theme_minimal()
-####################################################################################
-
