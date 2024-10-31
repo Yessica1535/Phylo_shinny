87d225038ec260472e9f7bd83f967e2e8ea76a10
Yessica1535 <1907553f@umich.mx>
2024-10-31 10:46:52 -0600

    Update Phylo_shinny.R

--- code/Phylo_shinny.R
+++ code/Phylo_shinny.R
@@ -22,14 +22,29 @@ asv_long <- reshape2::melt(asv_data, id.vars = "ASV", variable.name = "Sample", 
 filtered_data <- asv_long[asv_long$ASV == "ASV_1", ]
 
 # Create a grouping variable for samples
-filtered_data$Group <- ifelse(filtered_data$Sample %in% c("zr2757_1V3V4", "zr2757_2V3V4"), "Group 1", "Group 2")
+filtered_data$Group <- ifelse(filtered_data$Sample %in% c("zr2757_2V3V4", "zr2757_8V3V4", "zr2757_9V3V4"), "Ag-NP1",
+                              ifelse(filtered_data$Sample %in% c("zr2757_10V3V4", "zr2757_6V3V4", "zr2757_5V3V4"), "Ag-NP2", 
+                                     "Ag-SU3"))
+
+filtered_data$GroupYear <- ifelse(filtered_data$Sample %in% c("zr2757_2V3V4", "zr2757_10V3V4", "zr2757_1V3V4"), "2017",
+                              ifelse(filtered_data$Sample %in% c("zr2757_8V3V4", "zr2757_6V3V4", "zr2757_3V3V4"), "2018", 
+                                     "2019"))
+
+# Crear una tabla de grupos para las muestras
+#groups_data <- data.frame(
+#  Sample = c("zr2757_2V3V4", "zr2757_10V3V4", "zr2757_1V3V4", 
+#             "zr2757_8V3V4", "zr2757_6V3V4", "zr2757_3V3V4", 
+#             "zr2757_5V3V4", "zr2757_9V3V4", "zr2757_7V3V4", "zr2757_4V3V4"),
+#  Group = c("Ag-NP1", "Ag-NP2", "Ag-SU3", "Ag-NP1", "Ag-NP2", 
+#            "Ag-SU3", "Ag-NP2", "Ag-NP1", "Ag-SU3", "Ag-SU3")
+#)
 
 # Generate the boxplot using ggplot
 ggplot(filtered_data, aes(x = Group, y = Read_Count, fill = Group)) +
   geom_boxplot() +
   labs(title = paste("Boxplot for", "ASV_1"),
        x = "Group", y = "Read Count") +
-  scale_fill_manual (values = c("Group 1" = "#B57EDC", "Group 2" = "#BFF7DC")) + theme_minimal()
+  scale_fill_manual (values = c("Ag-NP1" = "#B57EDC", "Ag-NP2" = "#BFF7DC", "Ag-SU3" = "#FFC0CB")) + theme_minimal()
 ################################################################################################################
 
 # Define UI for the Shiny app
@@ -49,21 +64,30 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$boxplot <- renderPlot({
-    
+
     # Filter data for the selected OTU
     filtered_data <- asv_long[asv_long$ASV == input$selected_asv, ]
     
     # Create a grouping variable for samples
-    filtered_data$Group <- ifelse(filtered_data$Sample %in% c("zr2757_1V3V4", "zr2757_2V3V4"), "Group 1", "Group 2")
+    filtered_data$Group <- ifelse(filtered_data$Sample %in% c("zr2757_2V3V4", "zr2757_8V3V4", "zr2757_9V3V4"), "Ag-NP1",
+                                  ifelse(filtered_data$Sample %in% c("zr2757_10V3V4", "zr2757_6V3V4", "zr2757_5V3V4"), "Ag-NP2", 
+                                         "Ag-SU3"))
+    
+    filtered_data$GroupYear <- ifelse(filtered_data$Sample %in% c("zr2757_2V3V4", "zr2757_10V3V4", "zr2757_1V3V4"), "2017",
+                                      ifelse(filtered_data$Sample %in% c("zr2757_8V3V4", "zr2757_6V3V4", "zr2757_3V3V4"), "2018", 
+                                             "2019"))
     
     # Generate the boxplot using ggplot
     ggplot(filtered_data, aes(x = Group, y = Read_Count, fill = Group)) +
       geom_boxplot() +
       labs(title = paste("Boxplot for", "ASV_1"),
            x = "Group", y = "Read Count") +
-      scale_fill_manual (values = c("Group 1" = "#B57EDC", "Group 2" = "#BFF7DC")) + theme_minimal()
+      scale_fill_manual (values = c("Ag-NP1" = "#B57EDC", "Ag-NP2" = "#BFF7DC", "Ag-SU3" = "#FFC0CB")) + theme_minimal()
   })
 }
 
 # Run the app
 shinyApp(ui = ui, server = server)
+
+#Falta ver porque se corta hasta 1000 ASV
+
